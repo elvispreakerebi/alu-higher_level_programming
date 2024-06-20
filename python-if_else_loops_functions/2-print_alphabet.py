e5d4b6425186aa59eb8eab84ba93723b4f31c059
elvispreakerebi <e.kerebi@alustudent.com>
2024-06-20 05:19:25 -0700

    Fixed final error for script 2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 
-print("".join("{}".format(chr(letter)) for letter in range(ord('a'), ord('z') + 1)), end="")
+print("".join("{}".format(chr(letter)) 
+              for letter in range(ord('a'), ord('z') + 1)), end="")
