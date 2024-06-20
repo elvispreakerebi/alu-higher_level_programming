8ea2e6882d74528f950fd84baf33b44025a0de20
elvispreakerebi <e.kerebi@alustudent.com>
2024-06-20 05:42:16 -0700

    fixed script 5 error again

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-# Loop through numbers from 0 to 99
+# Print numbers 00 to 99 in the specified format
 for i in range(100):
-    # Format each number to have two digits
-    formatted_number = "{:02}".format(i)
-    
-    # Print the formatted number followed by ", " except for the last number
-    if i < 99:
-        print(formatted_number + ", ", end="")
+    if i < 10:
+        print("0{}, ".format(i), end="")
+    elif i == 99:
+        print("{:02}".format(i))
     else:
-        print(formatted_number)  # Print the last number with a newline
+        print("{:02}, ".format(i), end="")
