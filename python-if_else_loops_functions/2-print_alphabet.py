3e5cbf4addf5cac7b8a114384162ad69a45dae24
elvispreakerebi <e.kerebi@alustudent.com>
2024-06-20 05:21:08 -0700

    Fixed whitespace error

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 print("".join("{}".format(chr(letter)) 
-              for letter in range(ord('a'), ord('z') + 1)), end="")
+    for letter in range(ord('a'), ord('z') + 1)), end="")
