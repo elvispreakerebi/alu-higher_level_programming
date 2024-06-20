0fecf83568ebdf5950b4441277a89e2acd6a56fe
elvispreakerebi <e.kerebi@alustudent.com>
2024-06-20 06:34:19 -0700

    Script 9

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    last_digit = abs(number) % 10  # Get the last digit using modulus operator
+    print(last_digit)  # Print the last digit
+    return last_digit  # Return the last digit
+
+# Test cases (for standalone testing)
+if __name__ == "__main__":
+    print_last_digit(98)
+    print_last_digit(0)
+    r = print_last_digit(-1024)
+    print(r)
