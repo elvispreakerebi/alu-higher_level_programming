5a429c07badecc081013ee9fe8e5cf0375be1895
elvispreakerebi <e.kerebi@alustudent.com>
2024-06-20 05:57:38 -0700

    Fixed script 6 error

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 # Print all possible combinations of two digits in ascending order
 for tens in range(9):  # tens place digit ranges from 0 to 8
     for units in range(tens + 1, 10):  # units place digit ranges from (tens + 1) to 9
-        print("{:d}{:d}, ".format(tens, units), end="")
-
-# Print the last combination without the trailing comma and space
-print("{:d}{:d}".format(8, 9))
+        if tens == 8 and units == 9:
+            print("{:d}{:d}".format(tens, units))  # Print the last combination without ", "
+        else:
+            print("{:d}{:d}, ".format(tens, units), end="")
