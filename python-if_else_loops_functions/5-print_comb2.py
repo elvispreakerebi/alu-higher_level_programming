45b355e903cdb5c50f8b750aca6c73533d8ec5f2
elvispreakerebi <e.kerebi@alustudent.com>
2024-06-20 05:44:07 -0700

    fixed script 5 error about print

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,9 +2,7 @@
 
 # Print numbers 00 to 99 in the specified format
 for i in range(100):
-    if i < 10:
-        print("0{}, ".format(i), end="")
-    elif i == 99:
-        print("{:02}".format(i))
-    else:
+    if i < 99:
         print("{:02}, ".format(i), end="")
+    else:
+        print("{:02}".format(i))
