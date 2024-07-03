92cd3664070375e5f7ef568a6eb4a456750aa6d5
elvispreakerebi <e.kerebi@alustudent.com>
2024-07-03 01:57:20 -0700

    script 4 error fixed

--- python-classes/4-main.py
+++ python-classes/4-main.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Script to test the Square class."""
+"""
+This script demonstrates the use of the Square class.
+
+The Square class allows for the creation of square objects with a private
+size attribute, which can be accessed and modified through getter and setter
+methods. The class also includes a method to calculate the area of the square.
+"""
 
 Square = __import__('4-square').Square
 
-def main():
-    """Main function to demonstrate the usage of the Square class."""
-    my_square = Square(89)
+# Create a Square instance with size 89
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+# Change the size of the square to 3
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+# Attempt to set the size to a non-integer value to demonstrate error handling
+try:
+    my_square.size = "5 feet"
     print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-    my_square.size = 3
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-    try:
-        my_square.size = "5 feet"
-        print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-    except Exception as e:
-        print(e)
-
-if __name__ == "__main__":
-    main()
+except Exception as e:
+    print(e)
