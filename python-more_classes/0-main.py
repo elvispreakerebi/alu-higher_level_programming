ad94016be1b1596ec048883a0ebda1939652c0a0
elvispreakerebi <e.kerebi@alustudent.com>
2024-07-06 10:42:28 -0700

    script 0 error fixed

--- python-more_classes/0-main.py
+++ python-more_classes/0-main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""
+This script tests the Rectangle class from the module 0-rectangle.
+"""
+
 Rectangle = __import__('0-rectangle').Rectangle
 
-my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-
+"""
+This module defines an empty class Rectangle that defines a rectangle.
+"""
 
 class Rectangle:
+    """
+    An empty class that defines a rectangle.
+    """
     pass
