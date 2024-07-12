55970d8c5d3ac283dad01eaf0ff67bc9a6517c9c
elvispreakerebi <e.kerebi@alustudent.com>
2024-07-12 12:51:59 -0700

    script 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This is the script """
+
+class BaseGeometry:
+    """
+    BaseGeometry class with a method to calculate the area.
+    """
+    def area(self):
+        """
+        Raises an Exception to indicate that the area method is not implemented.
+        """
+        raise Exception("area() is not implemented")
