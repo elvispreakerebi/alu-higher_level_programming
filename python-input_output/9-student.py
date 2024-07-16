e919bd118b22c574503e68dec415ca7c5a0f8f9e
elvispreakerebi <e.kerebi@alustudent.com>
2024-07-16 01:47:34 -0700

    script 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" This is the script """
+
+
+class Student:
+    """
+    A class that defines a student with attributes: first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first_name, last_name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary containing the attributes of the Student instance.
+        """
+        # Create a dictionary representation of the instance
+        json_dict = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+        return json_dict
