28a10265232559e88f60ce5ae1d9cbc52ae06f6a
elvispreakerebi <e.kerebi@alustudent.com>
2024-07-16 01:28:45 -0700

    script 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,16 +4,13 @@
 
 def read_file(filename=""):
     """
-    Reads a UTF-8 encoded text file and prints its content to stdout.
+    Reads a text file (UTF8) and prints it to stdout.
 
-    Parameters:
-    filename (str): The name of the file to read. Defaults to an empty string.
+    Args:
+        filename (str): The path to the text file to be read. Defaults to an empty string.
 
     Returns:
-    None
+        None
     """
-    with open(filename, "r", encoding="utf-8") as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         print(file.read())
-
-# Example usage:
-# read_file("example.txt")
