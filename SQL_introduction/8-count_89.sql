d0eef3e8e7d8441af7a6840a7d5e3a6fbff65d49
elvispreakerebi <e.kerebi@alustudent.com>
2024-07-20 09:35:22 -0700

    script 8

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- Script to display the number of records with id = 89 in the table first_table of the database hbtn_0c_0
+
+-- Count the number of records with id = 89
+SELECT COUNT(*) AS record_count
+FROM first_table
+WHERE id = 89;
