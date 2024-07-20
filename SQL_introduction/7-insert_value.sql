6fc6329c489cb31f478873f261860c97a42a2d61
elvispreakerebi <e.kerebi@alustudent.com>
2024-07-20 09:34:18 -0700

    script 7

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Script to insert a new row into first_table with id=89 and name='Best School' in the specified database
+
+-- INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School');
