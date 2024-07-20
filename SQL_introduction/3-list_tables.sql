9214ec7124b7181cfecdf7b68acc27fc3c4b15ef
elvispreakerebi <e.kerebi@alustudent.com>
2024-07-20 09:27:27 -0700

    script 3

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,28 +1,13 @@
--- This script lists all the tables of a database in your MySQL server
--- The database name will be passed as an argument of the mysql command
--- If the database hbtn_0c_0 doesn’t exist, the script should not fail
--- SQL keywords are in uppercase
+-- Task: List all tables of a database in MySQL server
+-- This SQL script lists all the tables in the specified database
 
--- Use the provided database
+-- Use the database specified as an argument
 USE mysql;
 
--- Ensure the script does not fail if the database does not exist
--- The following statement will only create the procedure if the database exists
-DELIMITER //
-
-CREATE PROCEDURE list_tables (IN db_name VARCHAR(255))
-BEGIN
-  DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN END;
-  SET @stmt = CONCAT('SELECT table_name FROM information_schema.tables WHERE table_schema = "', db_name, '"');
-  PREPARE stmt FROM @stmt;
-  EXECUTE stmt;
-  DEALLOCATE PREPARE stmt;
-END //
-
-DELIMITER ;
-
--- Call the procedure to list tables from the specified database
-CALL list_tables('hbtn_0c_0');
-
--- Drop the procedure after use to clean up
-DROP PROCEDURE IF EXISTS list_tables;
+-- Query to list all tables in the current database
+SELECT 
+    TABLE_NAME 
+FROM 
+    INFORMATION_SCHEMA.TABLES 
+WHERE 
+    TABLE_SCHEMA = 'mysql';
