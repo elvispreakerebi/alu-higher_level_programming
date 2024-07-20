f9c859e56ddac4958a74d9337c1578edf4c75c59
elvispreakerebi <e.kerebi@alustudent.com>
2024-07-20 09:33:10 -0700

    script 6

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- Script to list all rows of the table first_table from the database hbtn_0c_0
+-- The database name will be passed as an argument of the mysql command
+
+-- Select all fields from the first_table
+SELECT * FROM first_table;
