399f29863e36596b63e0ada1bec309aa557e6e1c
elvispreakerebi <e.kerebi@alustudent.com>
2024-08-02 02:01:13 -0700

    script 5 fixed

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -3,9 +3,7 @@
 -- If the table already exists, the script will not fail
 
 -- Check if the table id_not_null exists, if not create it
-CREATE TABLE IF NOT EXISTS id_not_null (
-    -- id column definition
-    id INT DEFAULT 1,
-    -- name column definition
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT UNIQUE DEFAULT 1,
     name VARCHAR(256)
 );
