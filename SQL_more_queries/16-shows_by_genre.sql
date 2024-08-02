8ad2e11bdf3e12a24bd4bd355a6d60ab7008312b
elvispreakerebi <e.kerebi@alustudent.com>
2024-08-02 01:53:15 -0700

    script 16 fixed

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -4,7 +4,7 @@
 -- Results must be sorted in ascending order by the show title and genre name
 -- Use only one SELECT statement
 
-SELECT tv_shows.title, tv_genres.name AS genre
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
