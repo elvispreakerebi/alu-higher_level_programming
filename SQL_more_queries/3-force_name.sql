4c000fbd413e1000082ef357a3505297d4e6dfbb
elvispreakerebi <e.kerebi@alustudent.com>
2024-08-02 02:04:51 -0700

    script 3 fixed

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,11 +1,14 @@
 -- Script to create the table force_name on MySQL server
 -- The script ensures the table is created only if it doesn't already exist
 
--- Use the database provided as an argument
-USE your_database_name;
-
--- Check if the table force_name exists and create it if it doesn't
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
+
+-- Insert sample data
+INSERT INTO force_name (id, name) VALUES (1, 'Holberton School');
+INSERT INTO force_name (id, name) VALUES (1, 'Python is cool');
+INSERT INTO force_name (id, name) VALUES (2, 'Holberton');
+INSERT INTO force_name (id, name) VALUES (3, 'School');
+INSERT INTO force_name (id, name) VALUES (4, 'C is fun');
