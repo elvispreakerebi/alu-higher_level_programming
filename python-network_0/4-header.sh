295c779465c83e5c143b2af5c8aa52579e30aa4a
elvispreakerebi <e.kerebi@alustudent.com>
2024-08-02 02:42:59 -0700

    script 4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL with a custom header and displays the body of the response
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -v -H "X-HolbertonSchool-User-Id: 98" "$1" 2>&1 | grep -E '^< '
