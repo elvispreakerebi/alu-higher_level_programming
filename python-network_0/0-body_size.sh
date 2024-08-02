ac27a624593a356aa540551c048a0c9323cefaee
elvispreakerebi <e.kerebi@alustudent.com>
2024-08-02 02:24:53 -0700

    fixed script 0

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 # Check if URL is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-URL=$1
-
-# Send a request to the URL and display the size of the body in bytes
-curl -s -o /dev/null -w "%{size_download}\n" "$URL"
-
+#!/bin/bash
+curl -s "$1" -o response.txt
+wc -c < response.txt
