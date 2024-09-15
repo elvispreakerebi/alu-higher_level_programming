9c136fd870ee086c57cf8bd127904cad972a493d
elvispreakerebi <e.kerebi@alustudent.com>
2024-09-15 06:07:34 -0700

    fixed script 7 error

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,10 +4,10 @@ const str = 'C is fun';
 const args = process.argv.slice(2);
 let num = args[0];
 
-if (args.length === 0) {
+if (num === undefined) {
   console.log('Missing number of occurrences');
 } else {
-  while (num !== 0) {
+  while (num > 0) {
    console.log(str);
    num--;
   }
