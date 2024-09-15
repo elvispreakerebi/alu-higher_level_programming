032eeb21f4c1a1ef24474110d9ac41de48312b1e
elvispreakerebi <e.kerebi@alustudent.com>
2024-09-15 02:58:59 -0700

    script 2 error fixed finally

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-function argFinder (...args) {
-  if (args.length === 0) {
-    console.log('No argument');
-  } else if (args.length === 1) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
+const args = process.argv.slice(2);  // Get arguments passed to the script, excluding the first two default ones.
+
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
